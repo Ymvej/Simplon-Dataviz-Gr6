53ffb09b742c5c349a8ac7cce373554ea681c43f
Ymvej <samuel.queyrichon@gmail.com>
2020-03-05 18:58:01 +0100

    Les maps pas finies

---
+++ wow.py
@@ -0,0 +1,126 @@
+
+
+import pandas as pd
+import time
+import os
+import pygal
+from pygal.style import NeonStyle
+from progress.bar import Bar
+
+
+Col_avantage = ['ligne_identifiant', 'denomination_sociale', 'categorie', 'qualite', 'benef_codepostal', 'benef_ville', 'pays', 'benef_titre_libelle', 'benef_speicalite_libelle', 'benef_etablissement_codepostal', 'ligne_type', 'avant_date_signature', 'avant_montant_ttc']
+Col_convention = ['ligne_identifiant', 'denomination_sociale', 'categorie', 'qualite', 'benef_codepostal', 'benef_ville', 'pays', 'benef_titre_libelle', 'benef_speicalite_libelle', 'benef_etablissement_codepostal', 'ligne_type', 'conv_date_signature', 'conv_montant_ttc']
+Col_remuneration = ['entreprise_identifiant', 'denomination_sociale', 'benef_categorie_code', 'qualite', 'benef_codepostal', 'pays', 'benef_titre_libelle', 'benef_speicalite_libelle', 'benef_etablissement_codepostal', 'remu_date', 'remu_montant_ttc']
+Col_entreprise = ['pays','secteur','code_postal','ville']
+
+
+
+start = time.perf_counter() # getting starting timestamp
+print('Starting import...\n\n')
+print('Importing D_avantage...')
+D_avantage = pd.read_csv("Base/declaration_avantage_2020_02_19_04_00.csv", sep = ";", usecols = Col_avantage)
+D_avantage.name = 'D_avantage'
+print('D_avantage successfully imported. 3 more to go.')
+print('Importing D_Convention...')
+D_Convention = pd.read_csv("Base/declaration_convention_2020_02_19_04_00.csv", sep = ";", usecols = Col_convention)
+D_Convention.name = 'D_Convention'
+print('D_Convention successfully imported. 2 more to go.')
+print('Importing D_Remuneration...')
+D_Remuneration = pd.read_csv("Base/declaration_remuneration_2020_02_19_04_00.csv", sep = ";", usecols = Col_remuneration)
+D_Remuneration.name = 'D_Remuneration'
+print('D_Remuneration successfully imported. 1 more to go.')
+print('Importing Entreprise...')
+Entreprise = pd.read_csv("Base/entreprise_2020_02_19_04_00.csv", sep = ",", usecols = Col_entreprise)
+Entreprise.name = 'Entreprise'
+print('Entreprise successfully imported.')
+success = time.perf_counter() # getting ending timestamp
+import_time = int(success - start) 
+print('All csv successfully imported in %s seconds.'%(import_time))
+
+
+
+
+
+def comparator3000(df, fetch):
+    '''
+
+    '''
+    print('Started importing %s from %s.'%(fetch, df.name) )
+    dic = dict()
+    # bar = Bar('Processing...', suffix='%(percent)d%%')
+    start = time.perf_counter()
+    q = list(df.index)
+    fc = 0
+    sc = 0
+    for i in q:
+        if i % int((len(q)/100)) == 0:
+            aa = int(i / len(q) * 100) 
+            aa = str(aa)
+            print('%s %% processed.'%(aa))
+
+        cp = df['benef_codepostal'][i]
+        ttc = df[fetch][i]
+
+        cp = str(cp)
+        cp = cp[:2]
+    
+        try:
+            cp = int(cp)
+            ttc = int(ttc)
+            sc += 1
+        except ValueError:
+            fc += 1
+            continue
+
+        if cp in dic:
+            dic[cp] += ttc
+
+        else:
+            dic[cp] = ttc
+
+    #         bar.next()
+    # bar.finish()
+
+    success = time.perf_counter()
+    ns = int(success - start) 
+
+    print('Succesfully imported %s from %s in %s seconds | %s rows had one or more missing values and were omitted | %s rows were usable'%(fetch, df.name, ns, sc, fc))        
+
+    return dic
+
+
+
+def get_map(dic, title, subtitle):
+    fr_chart = pygal.maps.fr.Departments(human_readable=True, width=1080, height=1080, style=NeonStyle)
+    fr_chart.title = str(title)
+    fr_chart.add(str(subtitle), dic)
+    fr_chart.render_in_browser()
+
+# jpp2 = comparator3000(D_Convention, 'conv_montant_ttc')
+jpp = comparator3000(D_avantage, 'avant_montant_ttc')
+# jpp3 = comparator3000(D_Remuneration, 'remu_montant_ttc')
+
+get_map(jpp, 'Test', 'Moins test')
+
+
+
+# array_ = []
+
+# def aggregator3000(array, df, fetch):
+
+#     for i in enumerate(fetch):
+#         print(i)
+#         print(list_fetch)
+#         fetched = comparator3000(df, fetch)
+#         array.append(fetched)
+    
+    
+
+# print(array_)
+# aggregator3000(array_, D_avantage, ['avant_montant_ttc'])
+# aggregator3000(array_, D_Convention, ['conv_montant_ttc'])
+# aggregator3000(array_, D_Remuneration, ['remu_montant_ttc'])
+# print(array_)
+
+# for i in array_:
+#     get_map(array_[i], 'Titletest', 'Subtitletest')
